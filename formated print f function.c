4b4d7916b446f8df2e0b021d0759959a51f66bb0
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 17:57:38 +0530

    Add files via upload

---
+++ formated print f function.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+
+{
+/*first example
+printf("my name is krishna kothiya");*/
+
+/*second example
+int a = 1000;
+float b = 2.5,sum;
+sum = a+b;
+printf("%f",sum);*/
+
+
+/*third example*/
+int a = 10;
+int b = 20;
+int sum;
+sum = a+b;
+printf("%d", sum);
+return 0;
+
+
+}
