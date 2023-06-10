a57a03c75ab774b0b0c0693cedb67460e8959be9
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 20:43:26 +0530

    Add files via upload

---
+++ unformated getchar & putchar function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+/*getchar & putchar example :1
+char sample;
+sample = getchar();
+putchar(sample);*/
+
+/*getchar & putchar example :
+char result = 'p';
+
+printf("Please Enter the charater");
+ result= getchar();
+
+printf("Entered charater");
+putchar(result);*/
+
+/* Getchar and Putchar Example :3*/
+char c;
+
+printf("enter the charater");
+c = getchar();
+
+printf("entered charater");
+putchar(c);
+return 0;
+
+}
---
+++ unformated gets & puts function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+char day[20];
+
+printf("Enter your day\n");
+gets(day);
+
+printf("Today is ");
+puts(day);
+break;
+
+
+}
