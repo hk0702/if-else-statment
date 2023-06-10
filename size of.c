c79b327c480c6bf85312806c9883a37a19868f5e
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 18:10:10 +0530

    Add files via upload

---
+++ size of.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+int a;
+char b;
+float c;
+double d;
+printf("%d",sizeof(a));
+printf("\n%d",sizeof(b));
+printf("\n%d", sizeof(c));
+printf("\n%d", sizeof(d));
+return 0;
+
+
+}
