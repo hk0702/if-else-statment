5dc387c8d2372b2b26bf2d05482c8943958e6329
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 18:05:52 +0530

    Add files via upload

---
+++ formate specifire.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a = 6;
+float b = 7.333;
+/*example :1*/
+printf("the value of a is %d and the value of b is %f", a, b\n);
+
+/*example :2*/
+printf("%18.4f ", b);
+
+/*example :3*/
+printf("%-18.5f krishna", );
+
+
+
+
+
+}
