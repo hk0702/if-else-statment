531f34cb11189ae0e89c5cde2a86df131287ea24
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 17:03:55 +0530

    Update c language task.c

--- c language task.c
+++ c language task.c
@@ -2,17 +2,15 @@
 int main()
 {
     int int1, int2;
-    int1 = 15;
-    int2 = 15;
 
     printf("Input the values for int1 and int2 : ");
     scanf("%d %d", &int1, &int2);
     if (int1 == int2)
     {
-        printf("int1 and int2 are equal\n");
+        printf("weather is equal");
         }
     else
     {
-        printf("int1 and int2 are not equal\n");
+        printf("weather are not equal\n");
 }
 }
