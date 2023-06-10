e62d7fa99f76b7d4fdd06f7fcc1acd1581206818
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 20:35:48 +0530

    Add files via upload

---
+++ switch statement.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter your Age\n");
+scanf("%d",&age);
+
+switch(age)
+{
+case 3:
+printf("the age is 3\n");
+
+
+case 13:
+printf("the age is 13\n");
+break;
+
+case 23:
+printf("the age is 23\n");
+break;
+
+default:
+printf("invalid Command");
+break;
+
+
+}
+return 0;
+
+}
+
+
+
+
+
+
+
