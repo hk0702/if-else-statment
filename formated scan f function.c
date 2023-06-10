1d147f88d5bb47d4f21c47fc03d3c1665443763d
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-10 18:00:25 +0530

    Add files via upload

---
+++ formated scan f function.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+/*
+int a;
+float b, sum;
+printf("enter value of a=");
+scanf("%d", &a);
+
+printf("enter value of b=");
+scanf("%f", &b);
+sum = a+b;
+printf("%f", sum);
+return 0;*/
+
+int a;
+float b, sum;
+
+printf("enter values of  a and b");
+scanf("%d%f", &a,&b);
+
+sum=a+b;
+printf("%f", sum);
+return 0;
+
+}
