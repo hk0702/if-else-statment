d0ce4620789927116a635a110963b8a8b8bf7f70
hk0702 <136152937+hk0702@users.noreply.github.com>
2023-06-28 17:45:23 +0530

    Add files via upload

---
+++ 1D array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    /*example :
+    int i=0;
+    int marks[4];
+
+    marks[0] = 60;
+    marks[1] = 70;
+    marks[2] = 80;
+    marks[3] = 90;
+
+    for(i=0;i<4;i++)
+    {
+        printf("%d \n",marks[i]);
+    }*/
+/*example :2*/
+int i=0;
+int marks[4] = {10,20,30,40};
+    for(i = 0; i<4; i++)
+    {
+        printf("%d \n", marks[i]);
+    }
+
+    return 0;
+}
+
+
